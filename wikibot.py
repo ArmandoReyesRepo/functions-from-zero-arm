b359c4d4ff4c11e04c8fc65e657f04790377176a
ArmandoReyesRepo <85260874+ArmandoReyesRepo@users.noreply.github.com>
2023-10-10 00:51:14 +0000

    fixing formatting issues

--- wikibot.py
+++ wikibot.py
@@ -2,18 +2,13 @@ from mylib.bot import scrape
 import click
 
 
-
 @click.command()
-@click.option('--name', 
-              help='Web page we want to scrape.')
-
-
+@click.option("--name", help="Web page we want to scrape.")
 def cli(name):
-    result=scrape(name)
-    #print(result)
+    result = scrape(name)
+    # print(result)
     click.echo(click.style(f" {result} ", bg="white", fg="blue"))
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     cli()
-
-
